63a6ef503c58443de88fcc1d58fd8a1a60b76bc4
DeeMysterio <diksha@digithinkit.com>
2020-02-03 17:02:22 +0530

    fix: reveal necessary config info

--- inventory/config.py
+++ inventory/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = '29c98d8bb0aabaca5e846d61d7edd8fn'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
